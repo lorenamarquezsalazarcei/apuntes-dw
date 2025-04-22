63599fd308586bb2f46f1a18fa78f16b1e113c93
lorenamarquezsalazarcei <lorenamarquezsalazar@alumnos.cei.es>
2025-04-22 20:39:41 +0200

    proyecto final

--- proyectofinal/script.js
+++ proyectofinal/script.js
@@ -14,4 +14,28 @@
     track.style.transform = `translateX(-${index * ancho}px)`;
   });
 
-  
+//   Lightbox
+
+const galleryImages = document.querySelectorAll(".galeria-grid img");
+  const lightbox = document.getElementById("image-lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const closeBtn = document.querySelector(".lightbox .close");
+
+  galleryImages.forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.style.display = "flex";
+      lightboxImg.src = img.src;
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
+
+  // Cerrar al hacer clic fuera de la imagen
+  lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+      lightbox.style.display = "none";
+    }
+  });
+
